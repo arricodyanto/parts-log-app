8fd6413b0edacb8c79defe0058ac275a942a22c9
arricodyanto <arricohandyanto@gmail.com>
2024-01-25 07:32:57 +0700

    :heavy_plus_sign: add: charts section larapex

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Part;
 use App\Models\Vehicle;
 use App\Models\VehicleSpecification;
+use ArielMejiaDev\LarapexCharts\LarapexChart;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $vehicles = Vehicle::all();
         $selectedVehicle = Vehicle::findOrFail(request('vehicle_id', $vehicles->last()->id));
 
@@ -18,7 +20,39 @@ class HomeController extends Controller
         $vehicleSpecifications = $selectedVehicle->vehicleSpecifications;
         $parts = $selectedVehicle->parts;
 
-        return view('home', compact('vehicles', 'selectedVehicle', 'hours_meter'));
+        // Calculate $sortedParts
+        $selectedHM = request('picked_hm', 250); // default value 250 if not provided
+        $sortedParts = $parts
+            ->sortBy('hours_meter')
+            ->filter(function ($part) use ($selectedHM) {
+                return $part->hours_meter <= $selectedHM;
+            })
+            ->values();
+
+        // parts chart
+        $pieChart = (new LarapexChart)->pieChart()
+            ->setTitle('Parts Price (Pie)')
+            ->setSubtitle('HM '.$selectedHM)
+            ->setDataset($sortedParts->pluck('price')->toArray())
+            ->setLabels($sortedParts->pluck('part_desc')->toArray());
+        
+        $barChart = (new LarapexChart)->barChart()
+            ->setTitle('Parts Price (Pie)')
+            ->setSubtitle('HM '.$selectedHM)
+            ->setHeight(350)
+            ->setXAxis($sortedParts->pluck('part_desc')->toArray())
+            ->setDataset([
+                [
+                    'name'  =>  'Quantity',
+                    'data'  =>  $sortedParts->pluck('qty')->map(fn ($value) => (int)$value)->toArray()
+                ],
+                [
+                    'name'  =>  'Price ($)',
+                    'data'  =>  $sortedParts->pluck('price')->toArray()
+                ],
+            ]);
+        
+        return view('home', compact('vehicles', 'selectedVehicle', 'sortedParts', 'hours_meter', 'pieChart', 'barChart'));
     }
 
 }
