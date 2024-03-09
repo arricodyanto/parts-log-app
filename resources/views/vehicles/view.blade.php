8cc384299839f589da073fac6e1f8d7d942a9cba
arricodyanto <arricohandyanto@gmail.com>
2024-03-09 23:42:54 +0700

    :art: update: order of the vehicle table view

--- resources/views/vehicles/view.blade.php
+++ resources/views/vehicles/view.blade.php
@@ -29,8 +29,8 @@
                   <thead>
                     <tr class="bg-primary text-center text-white">
                       <th></th>
-                      <th>Name</th>
                       <th>Image</th>
+                      <th>Name</th>
                       <th>Specifications</th>
                       <th>Action</th>
                     </tr>
@@ -41,29 +41,29 @@
                       @foreach ($vehicles as $index => $vehicle)
                         <tr class="hover">
                             <td class="text-center">{{$index + 1}}</td>
-                            <td class="text-center">{{$vehicle->name}}</td>
                             <td class="flex justify-center">
                               <img src="{{ asset('/images/' . ($vehicle->vehicle_photo ?? 'default-image.jpg')) }}" alt="{{ $vehicle->name }}'s Image" class="max-w-64 aspect-[5/3] object-cover rounded-lg">
                             </td>
+                            <td class="text-center">{{$vehicle->name}}</td>
                             <td>
                               @foreach ($vehicle->vehicleSpecifications as $index => $item)
                                 <p>{{ $index + 1 }}. {{$item->specs}}: {{ $item->specs_value }}</p>
                               @endforeach
                             </td>
-                            <td class="">
+                            <td>
                               <div class="flex xs:flex-col md:flex-row gap-2 justify-center">
                                   <a href="{{route('vehicles.edit', ['vehicle' => $vehicle->id])}}">
                                       <button class="btn btn-sm btn-warning text-white w-full">
                                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                          </svg>                                        
+                                          </svg>
                                           <p class="xs:hidden lg:flex">Edit</p>
                                       </button>
                                   </a>
                                   <button class="btn btn-sm btn-error text-white" onclick="showDeleteDialog('{{ $vehicle->id }}')">
                                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                      </svg>                                    
+                                      </svg>
                                       <p class="xs:hidden lg:flex">Delete</p>
                                   </button>
                               </div>
